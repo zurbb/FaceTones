e4f72409c5872785ed1ace4e1416d68436accc79
yedidyatob <yedidyat@gmail.com>
2024-05-23 16:04:51 +0300

    started training.py

--- models/data_loader.py
+++ models/data_loader.py
@@ -5,58 +5,93 @@ from torchvision.io import read_image
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from PIL import Image
+from voice_to_vec import VoiceToVec
 
-class ImagesDataset(Dataset):
-    def __init__(self, images_dir, transform=None):
+IMAGE_SUFFIX = "_0.jpg"
+VOICE_SUFFIX = ".mp3"
+
+class ImagesVoicesDataset(Dataset):
+    def __init__(self, images_dir, voices_dir, transform=None, voice_transform=None):
         self.images_dir = images_dir
+        self.voices_dir = voices_dir
         self.transform = transform
-        self.images = os.listdir(images_dir)
+        self.voice_transform = voice_transform
+        image_files = os.listdir(images_dir)
+        voice_files = os.listdir(voices_dir)
+        image_ids = [img.split(IMAGE_SUFFIX)[0] for img in image_files]
+        voice_ids = [voice.split(VOICE_SUFFIX)[0] for voice in voice_files]
+        ids = list(set(image_ids).intersection(set(voice_ids)))
+        self.images = [f"{id}{IMAGE_SUFFIX}" for id in ids]
+        self.voices = [f"{id}{VOICE_SUFFIX}" for id in ids]
+        self.images_and_voices_file_names = list(zip(self.images, self.voices))
+
 
     def __len__(self):
-        return len(self.images)
+        return len(self.images_and_voices_file_names)
 
     def __getitem__(self, idx):
-        img_name = self.images[idx]
+        print("getitem")
+        img_name, voice_name = self.images_and_voices_file_names[idx]
         img_path = os.path.join(self.images_dir, img_name)
+        voice_path = os.path.join(self.voices_dir, voice_name)
         # image = read_image(img_path)
         image = Image.open(img_path)
         if self.transform:
             image = self.transform(image)
-        label = self._img_file_name_to_label(img_name)
-        return image #, label
+        voice_embedding = self.voice_transform(voice_path)
+        return image , voice_embedding
     
-    def _img_file_name_to_label(self, img_name):
-        return "".join(img_name.split('.')[0].split('_')[:-1])
+
 # Custom collate function
 def custom_collate_fn(batch):
-    return torch.stack(batch)
+    print("custom_collate_fn")
+    images = torch.stack([item[0] for item in batch])
+    voices = torch.stack([item[1] for item in batch])
+    return images, voices
+
+# Create an instance of the ImagesDataset class
+transform = transforms.Compose([
+    transforms.Resize((128,128)),
+    transforms.ToTensor()
+])
+
+def get_train_loader(images_dir, voices_dir, batch_size=4, shuffle=True, num_workers=4):
+    voice_embedder = VoiceToVec()
+    voice_transform = voice_embedder.get_embedding 
+    train_dataset = ImagesVoicesDataset(images_dir, voices_dir, transform=transform, voice_transform=voice_transform)
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, collate_fn=custom_collate_fn)
+    return train_loader
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
 
     # Specify the directory containing the images
     images_dir = "data/train/images"
+    voices_dir = "data/train/audio"
     
-    # Create an instance of the ImagesDataset class
-    transform = transforms.Compose([
-        transforms.Resize((128,128)),
-        transforms.ToTensor()
-    ])
-    train_dataset = ImagesDataset(images_dir, transform=transform)
+
+    voice_embedder = VoiceToVec()
+    voice_transform = voice_embedder.get_embedding 
+    train_dataset = ImagesVoicesDataset(images_dir, voices_dir, transform=transform, voice_transform=voice_transform)
     
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4, collate_fn=custom_collate_fn)
 
     # Iterate over DataLoader
-    for batch in train_loader:
+    for batch, audios in train_loader:
         print("Batch Tensor Shape:", batch.size()) 
+        print("Audio Tensor Shape:", audios.size())
         break
     train_iter = iter(train_loader)
     first_batch = next(train_iter)
     second_batch = next(train_iter)
-    sample_images = second_batch
+    sample_images = second_batch[0]
     print("Sample images shape:", sample_images.shape)
     # print("Sample labels:", sample_labels)
     img = sample_images[1].squeeze().permute(1, 2, 0)
+
+
+    import matplotlib.pyplot as plt
+
     plt.imshow(img)
     plt.savefig("sample_image.png")
     plt.show()
---
+++ models/training.py
@@ -0,0 +1,98 @@
+import torch
+
+import torch.nn as nn
+import torch.optim as optim
+from data_loader import get_train_loader
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+# Define your neural network architecture
+class ImageVoiceClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(3*128*128, 64*64),
+            nn.ReLU(),
+            nn.Linear(64*64, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512)
+        )
+        
+    def forward(self, x):
+        print(f"Input shape: {x.shape}")
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+
+# Define your loss function
+def cosine_similarity_loss(outputs, voices):
+    cosine_similarity = nn.CosineSimilarity(dim=1)(outputs, voices)
+    loss = 1 - cosine_similarity.mean()  # subtract from 1 to make it a minimization problem
+    return loss
+
+
+# Load and preprocess your "imagesVoices" dataset
+# Split it into training and validation sets
+def train(train_data_loader, model, loss_fn, optimizer, num_epochs=1):
+    size = len(train_data_loader.dataset)
+    # Training loop
+    # for epoch in range(num_epochs):
+    for Batch_number, (images, voices) in enumerate(train_data_loader):
+        print(f"Batch number: {Batch_number}")
+        print(f"Images shape: {images.shape}")
+        # Forward pass
+        outputs = model(images)
+        loss = loss_fn(outputs, voices)
+
+        if Batch_number % 1 == 0:
+            loss, current = torch.mean(loss), (Batch_number + 1) * len(images)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        # Backward and optimize
+        print("Backward and optimize")
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print("Backward and optimize done")
+        if Batch_number == 10:
+             break
+    
+
+
+        # # Validate your model on the validation set
+        # with torch.no_grad():
+        #     for images, voices in validation_loader:
+        #         outputs = model(images)
+        #         val_loss = loss_fn(outputs, voices)
+        #         print(f"Validation Error: {val_loss.item():>7f}")
+
+
+        # Print training and validation metrics
+
+
+if __name__ == '__main__':
+    # Your code here
+
+    # Create an instance of your network
+    model = ImageVoiceClassifier().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    print(model)
+
+    # Load your dataset
+    images_dir = "data/train/images"
+    voices_dir = "data/train/audio"
+    train_dataset = get_train_loader(images_dir, voices_dir, batch_size=2)
+    train(train_dataset, model, cosine_similarity_loss, optimizer, num_epochs=1)
+
+    # Save your trained model
+    torch.save(model.state_dict(), 'image_voice_classifier.pth')
