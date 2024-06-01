cd42260302fb57f33af6dbe9df2c03c91921a060
zurbb <zur.binyamini@mail.huji.ac.il>
2024-06-01 19:20:35 +0300

    eval, need to finsh

---
+++ models/eval_lib.py
@@ -0,0 +1,20 @@
+from training import ImageVoiceClassifier
+import torch
+from training import ROOT_DIR
+import os
+from data_loader import get_train_loader
+
+
+def load_model_by_checkpoint(checkpoint_path:str)->ImageVoiceClassifier:
+    model = ImageVoiceClassifier()
+    model.load_state_dict(torch.load(checkpoint_path))
+    return model
+
+def load_validation_data(limit_size:int, batch_size:int, use_dino:bool)->torch.utils.data.DataLoader:
+    test_images_dir = os.path.join(ROOT_DIR, "data/test/images")
+    test_voices_dir = os.path.join(ROOT_DIR, "data/test/audio")
+    validation_data = get_train_loader(images_dir=test_images_dir, audios_dir=test_voices_dir, batch_size=batch_size, limit_size=limit_size, dino=use_dino)
+    return validation_data
+
+def cosine_similarity_loss(predicted, true):
+    return torch.nn.functional.cosine_similarity(predicted, true)
--- models/eval_sxs.py
+++ models/eval_sxs.py
@@ -1,6 +1,25 @@
-from ..models import *
+from data_loader import get_train_loader
+import coloredlogs, logging
+import argparse
+from training import ROOT_DIR
+import os
+import eval_lib as lib
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--validation_size", type=int, default=128, help="Validation size of the dataset")
+parser.add_argument("--batch_size", type=int, default=16, help="Batch size of the dataset")
+parser.add_argument("--run_name", type=str, required=True, help="Name of the run")
+parser.add_argument("--epochs", type=int, default=1, help="Number of epochs to train the model")
+parser.add_argument("--model_checkpoint", type=str, required=True, help="Path to the model checkpoint file")
+args = parser.parse_args()
 
 
-if __name__ == '__main__':
+
+
+def main():
+   model =  
     
-    print("sdsd")
+
+    
+if __name__ == "__main__":
+    main()
