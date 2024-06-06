a3fcd56382c98e13cfc540374ea3541981d173d6
zurbb <zur.binyamini@mail.huji.ac.il>
2024-06-06 11:49:50 +0300

    add device

--- models/model_config_lib.py
+++ models/model_config_lib.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
 class ImageToVoice(nn.Module):
     
     def __init__(self):
