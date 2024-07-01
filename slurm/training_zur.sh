8eb43d9d11e16161d4f4c8a56a0c0b03cd4a6d77
zurbb <zur.binyamini@mail.huji.ac.il>
2024-07-01 12:40:54 +0300

    refactor: Update learning rate in training.py and slurm script

--- slurm/training_zur.sh
+++ slurm/training_zur.sh
@@ -3,15 +3,15 @@
 #SBATCH --mem=48gb
 #SBATCH -c48
 #SBATCH --time=4-12:00:00
-#SBATCH --error=logs/error_0107_vgg_style.txt
-#SBATCH --output=logs/107_vgg_style.txt
+#SBATCH --error=logs/error_0107_vgg_style_lr.txt
+#SBATCH --output=logs/0107_vgg_style_lr.txt
 #SBATCH --job-name=training_01
 umask 022
 /cs/ep/120/playground/Voice-Image-Classifier/.venv/bin/python3 /cs/ep/120/playground/Voice-Image-Classifier/models/training.py \
     --limit_size=100000 \
     --validation_size=1024 \
     --batch_size=16 \
-    --run_name=0107_vgg_style \
+    --run_name=0107_vgg_style_lr \
     --epochs=30 \
     --num_workers=11 \
-    --description="used vgg inspired nn, upper bond 0.9, lr 0.00001, removed dropout in the end"
+    --description="same as last expremint. but lr is 0.001" 
